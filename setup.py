59c383f252f62398be239adc3ce45aec00d617f7
cristian <cristian.ricchiuto97@gmail.com>
2021-04-28 12:37:17 +0200

    com

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-cristan-r", # Replace with your own username
+    name="xtest", # Replace with your own username
     version="0.0.1",
     author="Cristian",
     author_email="xraid.rfid.test01@gmail.com",
     description="test rfid package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/cristian-97/xtest",
     project_urls={
         "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
     },
@@ -23,7 +23,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "rfid"},
+    packages=setuptools.find_packages(where="rfid"),
     python_requires=">=3.6",
 ) 
